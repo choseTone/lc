ab3715dc4b6a14b1947ce518c34bd45c447e9674
wangqc <idealmaster@163.com>
2018-06-29 22:43:10 +0800

    first commit

---
+++ uci/lc119_180701.py
@@ -0,0 +1,26 @@
+__author__ = 'wangqc'
+
+'''
+119. Pascal's Triangle II
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+Note that the row index starts from 0.
+
+Example:
+Input: 3
+Output: [1,3,3,1]
+
+Follow up:
+Could you optimize your algorithm to use only O(k) extra space?
+'''
+
+class Solution:
+    def getRow(self, rowIndex):
+        res = [1]
+        for _ in range(rowIndex):
+            res = [1] + [res[i-1]+res[i] for i in range(1, len(res))] + [1]
+        return res
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print(sol.getRow(31))
