03ad22b98efa0e6d7a286cf85bee9c36b2a584d7
wangqc <idealmaster@163.com>
2018-06-30 17:56:27 +0800

    update 180702

---
+++ uci/lc20_180702.py
@@ -0,0 +1,54 @@
+__author__ = 'wangqc'
+
+import time
+
+'''
+20. Valid Parentheses
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+Input: "()"
+Output: true
+
+Example 2:
+Input: "()[]{}"
+Output: true
+
+Example 3:
+Input: "(]"
+Output: false
+
+Example 4:
+Input: "([)]"
+Output: false
+
+Example 5:
+Input: "{[]}"
+Output: true
+'''
+
+class Solution:
+    def isValid(self, s):
+        # using stack
+        stack = []
+        for i in s:
+            if i in {'{', '[', '('}:
+                stack.append(i)
+            elif stack:
+                front = stack.pop()
+                if front == '{' and i != '}' or front == '[' and i != ']' or front == '(' and i != ')':
+                    return False
+            else: return False
+        return not stack
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    t = time.time()
+    ans = sol.isValid('{[]}()')
+    print('ans: %s\ntime: %.2fms' % (ans, ((time.time() - t)) * 1000))
---
+++ uci/lc50_180702.py
@@ -0,0 +1,40 @@
+__author__ = 'wangqc'
+
+import time
+
+'''
+50. Pow(x, n)
+
+Implement pow(x, n), which calculates x raised to the power n (x^n).
+
+Example 1:
+Input: 2.00000, 10
+Output: 1024.00000
+
+Example 2:
+Input: 2.10000, 3
+Output: 9.26100
+
+Example 3:
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+
+Note:
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−2^31, 2^31 − 1]
+'''
+
+class Solution:
+    def myPow(self, x, n):
+        # recursive call
+        if n < 0: x, n = 1 / x, -n
+        return self.myPow(x, n - 1) * x if n % 2 else self.myPow(x * x, n // 2) if n else 1
+
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    t = time.time()
+    ans = sol.myPow(2, 10)
+    print('ans: %s\ntime: %.2fms' % (ans, ((time.time() - t)) * 1000))
