89fa889f115d1a93c19a9025168686dc9a0e3bf0
wangqc <idealmaster@163.com>
2018-07-01 11:14:22 +0800

    update 180703

---
+++ uci/lc190_180703.py
@@ -0,0 +1,36 @@
+__author__ = 'wangqc'
+
+from time import time
+
+'''
+190. Reverse Bits
+
+Reverse bits of a given 32 bits unsigned integer.
+
+Example:
+Input: 43261596
+Output: 964176192
+Explanation: 43261596 represented in binary as 00000010100101000001111010011100, 
+             return 964176192 represented in binary as 00111001011110000010100101000000.
+Follow up:
+If this function is called many times, how would you optimize it?
+'''
+
+class Solution:
+    # use basic bit operations and mask techniques
+    def reverseBits(self, n):
+        mask_8, mask_4, mask_2, mask_1 = 0xff00ff, 0xf0f0f0f0f, 0x33333333, 0x55555555
+        n = n << 16 | n >> 16
+        n = (n & mask_8) << 8 | (n >> 8) & mask_8
+        n = (n & mask_4) << 4 | (n >> 4) & mask_4
+        n = (n & mask_2) << 2 | (n >> 2) & mask_2
+        return (n & mask_1) << 1 | (n >> 1) & mask_1
+
+if __name__ == '__main__':
+    sol = Solution()
+    n = 2 ** 31 + 43261597
+    t = time()
+    ans = sol.reverseBits(n)
+    # before: 0b10000010100101000001111010011101
+    # after : 0b10111001011110000010100101000001
+    print('before: %s\nafter : %s\nans: %s\ntime: %.3fms' % (bin(n), bin(ans), ans, ((time() - t)) * 1000))
---
+++ uci/lc494_180703.py
@@ -0,0 +1,50 @@
+__author__ = 'wangqc'
+
+from time import time
+
+'''
+494. Target Sum
+
+You are given a list of non-negative integers, a1, a2, ..., an, and a target, S. Now you have 2 symbols + and -. For each integer, you should choose one from + and - as its new symbol.
+
+Find out how many ways to assign symbols to make sum of integers equal to target S.
+
+Example 1:
+Input: nums is [1, 1, 1, 1, 1], S is 3. 
+Output: 5
+Explanation: 
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+
+There are 5 ways to assign symbols to make the sum of nums be target 3.
+Note:
+The length of the given array is positive and will not exceed 20.
+The sum of elements in the given array will not exceed 1000.
+Your output answer is guaranteed to be fitted in a 32-bit integer.
+'''
+
+class Solution:
+    def findTargetSumWays(self, nums, S):
+        # S = A - B, A is the sum of all positive elements and B is that of the negative, sum of nums + S = 2A
+        # so use dp to find solution that the sum of selected positive elements is A or (sum of nums + S) / 2
+        sum_nums = sum(nums)
+        if S > sum_nums or (sum_nums + S) & 1: return 0
+        pos = (sum(nums) + S) >> 1
+        dp = [1] + [0] * pos
+        for n in nums:
+            for i in range(pos, n-1, -1):
+                dp[i] += dp[i - n]
+        return dp[pos]
+
+if __name__ == '__main__':
+    from random import randint
+    sol = Solution()
+    t = time()
+    nums, S = [randint(1, 100) for _ in range(20)], randint(1, 100)
+    ans = sol.findTargetSumWays(nums, S)
+    # nums: [19, 28, 77, 71, 33, 88, 83, 58, 59, 90, 28, 1, 53, 85, 22, 15, 37, 27, 21, 91] S: 50 ans: 3146
+    print('input:\nnums: %s\tS: %d\nans: %s\ntime: %.3fms' % (nums, S, ans, ((time() - t)) * 1000))
