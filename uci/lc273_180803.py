a2cc697f1c1108b7b85776e3586e5fccb782e616
wangqc <idealmaster@163.com>
2018-08-05 14:47:05 +0800

    update 180803

--- uci/lc273_180803.py
+++ uci/lc273_180803.py
@@ -25,27 +25,26 @@ Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Th
 
 class Solution:
     digit = {'0':'Zero','1':'One','2':'Two','3':'Three','4':'Four','5':'Five','6':'Six','7':'Seven','8':'Eight','9':'Nine',
-        '10':'Ten','11':'Eleven','12':'Twelve','13':'Thirteen','14':'Fourteen','15':'Fifteen','16':'Sixteen',
-        '17':'Seventeen','18':'Eighteen','19':'Nineteen','20':'Twenty','30':'Thirty','40':'Forty','50':'Fifty',
-        '60':'Sixty','70':'Seventy','80':'Eighty','90':'Ninety','100':'One Hundred'}
+             '10':'Ten','11':'Eleven','12':'Twelve','13':'Thirteen','14':'Fourteen','15':'Fifteen','16':'Sixteen',
+             '17':'Seventeen','18':'Eighteen','19':'Nineteen','20':'Twenty','30':'Thirty','40':'Forty','50':'Fifty',
+             '60':'Sixty','70':'Seventy','80':'Eighty','90':'Ninety','100':'One Hundred'}
     ty = {'2':'Twenty','3':'Thirty','4':'Forty','5':'Fifty','6':'Sixty','7':'Seventy','8':'Eighty','9':'Ninety'}
+    seg_level = ['Billion', 'Million', 'Thousand', '']
 
     def numberToWords(self, num):
-        num, s, ans = str(num), ['', 'Thousand', 'Million', 'Billion'], ''
-        if len(num) % 3: num = '0' * (3 - len(num) % 3) + num
-        n = len(num)
-        seg_index = n // 3 - 1
-        for seg in [self.convert(num[i: i+3].lstrip('0')) for i in range(0, n, 3)]:
-            if seg: ans += '%s %s ' % (seg.rstrip(), s[seg_index])
-            seg_index -= 1
+        num, ans = str(num), ''
+        if len(num) % 3:
+            num = '0' * (3 - len(num) % 3) + num
+        n = len(num) // 3
+        for i in range(n):
+            seg = self.convert_seg(num[3*i: 3*i+3].lstrip('0'))
+            if seg: ans += '%s %s ' % (seg, self.seg_level[4 - n + i])
         return ans.rstrip() or 'Zero'
 
-
-    def convert(self, num):
-        return num and \
-               (self.digit[num] if num in self.digit
-                else '%s %s' % (self.ty[num[0]], self.digit[num[1]]) if len(num) == 2
-               else '%s Hundred %s' % (self.digit[num[0]], self.convert(num[1:].lstrip('0'))))
+    def convert_seg(self, num):
+        return num and (self.digit[num] if num in self.digit
+                        else '%s %s' % (self.ty[num[0]], self.digit[num[1]]) if len(num) == 2
+                        else '%s Hundred %s' % (self.digit[num[0]], self.convert_seg(num[1:].lstrip('0')))).rstrip()
 
 
 if __name__ == '__main__':
@@ -53,5 +52,5 @@ if __name__ == '__main__':
 
     sol = Solution()
     t = time()
-    ans = sol.numberToWords(1234567891)
+    ans = sol.numberToWords(200000)
     print('ans: %s\ntime: %.3fms' % (ans, ((time() - t)) * 1000))
