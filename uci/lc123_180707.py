ef5b971dbb4a90a15efae8311188cbe5f77d43e6
wangqc <idealmaster@163.com>
2018-08-26 16:52:20 +0800

    update 180707

--- uci/lc123_180707.py
+++ uci/lc123_180707.py
@@ -1,7 +1,7 @@
 __author__ = 'wangqc'
 
 '''
-122. Best Time to Buy and Sell Stock II
+123. Best Time to Buy and Sell Stock III
 
 Say you have an array for which the ith element is the price of a given stock on day i.
 Design an algorithm to find the maximum profit. You may complete at most two transactions.
