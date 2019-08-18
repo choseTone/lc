61ef03c2a3c3a2d80e6839ac058e8d52fa84d004
wangqc <idealmaster@163.com>
2019-08-18 12:15:36 -0700

    add lv6

--- uci/19_summer/e2_tree/6_A67.py
+++ uci/19_summer/e2_tree/6_A67.py
@@ -9,12 +9,11 @@ class Solution:
         def count(N):
             if not N or (not d and N <= 10):
                 return 0
-            cnt = int(N % 10 > d)
-            if N // 10:
+            # cnt is d# caused by lowest digit
+            cnt = int(N % 10 > d) + N // 10 - (not d) # last digit is d
+            if N // 10:  # last digit is [0,N%10), previous digits contain d
                 cnt += str(N // 10).count(str(d)) * (N % 10)
-            cnt += N // 10 - (not d)
-            # cnt is d# from lowest digit,
-            # count(N//10)*10 is # from previous digits' d# + last digit as {0-9}
+            # count(N//10)*10 is d# caused by previous digits' d# + last digit as [0,9]
             return cnt + count(N // 10) * 10
 
         return count(high + 1) - count(low)
