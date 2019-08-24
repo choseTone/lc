899f0ac0cdea864dd0a2d55a7fdaa8257d870e18
wangqc <idealmaster@163.com>
2019-08-24 15:23:14 -0700

    add lv5

--- uci/19_summer/e3_ss/5_778.py
+++ uci/19_summer/e3_ss/5_778.py
@@ -4,20 +4,21 @@ __author__ = 'wangqc'
 
 import heapq
 
-
 class Solution:
     def swimInWater(self, grid):
-        N, pq, min_t = len(grid), [(grid[0][0], 0, 0)], 0
+        # search from (N-1,N-1) to (0,0)
+        # as min-heap tends to pop smaller x,y coordinates points
+        N, min_t = len(grid), grid[0][0]
+        pq = [(grid[N-1][N-1], N-1, N-1)]
         while pq:
             t, i, j = heapq.heappop(pq)
             min_t = max(t, min_t)
-            if i == j == N-1:
-                break
             for x, y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
+                if x == y == 0:
+                    return min_t
                 if 0 <= x < N and 0 <= y < N and grid[x][y] >= 0:
                     heapq.heappush(pq, (grid[x][y], x, y))
                     grid[x][y] = -1
-        return min_t
 
 
 if __name__ == '__main__':
