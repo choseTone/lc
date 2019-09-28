a5da8b81c815944003a3e652dc72c0f006b8f302
wangqc <idealmaster@163.com>
2019-09-27 23:42:56 -0700

    update 4_200

--- uci/19_summer/e2_tree/4_200.py
+++ uci/19_summer/e2_tree/4_200.py
@@ -6,27 +6,26 @@ __author__ = 'wangqc'
 
 class Solution:
     def numIslands(self, grid):
-        m, n = len(grid), len(grid) and len(grid[0])
+        M, N = len(grid), len(grid) and len(grid[0])
 
         def dfs(i, j):
-            if 0 <= i < m and 0 <= j < n and grid[i][j] == "1":
-                grid[i][j] = "0"
-                for x, y in ((i-1,j), (i+1,j), (i,j-1), (i,j+1)):
+            for x, y in ((i-1,j),(i+1,j),(i,j-1),(i,j+1)):
+                if 0 <= x < M and 0 <= y < N and grid[x][y] == "1":
+                    grid[x][y] = "0"
                     dfs(x, y)
-                return 1
-            return 0
+            return 1
 
         def bfs(i, j):
             q, grid[i][j] = [(i, j)], "0"
             for i, j in q:
                 for x, y in ((i-1,j), (i+1,j), (i,j-1), (i,j+1)):
-                    if 0 <= x < m and 0 <= y < n and grid[x][y] == "1":
+                    if 0 <= x < M and 0 <= y < N and grid[x][y] == "1":
                         grid[x][y] = "0"
                         q.append((x, y))
             return 1
 
-        # return sum(dfs(i,j) for i in range(m) for j in range(n))
-        return sum(bfs(i,j) for i in range(m) for j in range(n) if grid[i][j] == "1")
+        # return sum(dfs(i,j) for i in range(M) for j in range(N) if grid[i][j] == "1")
+        return sum(bfs(i,j) for i in range(M) for j in range(N) if grid[i][j] == "1")
 
 
 if __name__ == '__main__':
